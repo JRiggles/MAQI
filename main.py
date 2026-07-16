ca9ae40f9a59d93dec1e9fbb10f66b6c6a3b6a49
John Riggles <jriggles@icloud.com>
2026-07-15 22:07:32 -0400

    fix: update copyright year in MIT License frontmatter (duh)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2024-25 John Riggles [sudo_whoami]
+Copyright (c) 2026 John Riggles [sudo_whoami]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
